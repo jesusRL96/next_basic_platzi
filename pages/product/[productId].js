a6d7417d29f819340866b36447ff120672baee24
jesus <jesus.alberto@desici.com>
2022-01-20 12:42:57 -0600

    pagina detalle

--- pages/product/[productId].js
+++ pages/product/[productId].js
@@ -1,13 +1,113 @@
-import React from 'react'
-import { useRouter } from 'next/router'
+import React, { useState, useEffect } from "react";
+import { useRouter } from "next/router";
+import {
+  Box,
+  Container,
+  Stack,
+  Text,
+  Image,
+  Flex,
+  VStack,
+  Heading,
+  SimpleGrid,
+  StackDivider,
+  useColorModeValue,
+  List,
+  ListItem,
+} from "@chakra-ui/react";
+import { Spinner } from "@chakra-ui/react";
 
-const ProductItem = () => {
-    const {query: {productId}} = useRouter();
+export default function ProductItem() {
+  const {
+    query: { productId },
+  } = useRouter();
+  const [product, setProduct] = useState(null);
+  useEffect(() => {
+    if (productId) {
+      fetch(`http://localhost:3000/api/avo/${productId}`)
+        .then((response) => response.json())
+        .then((data) => {
+          setProduct(data);
+        });
+    }
+  }, [productId]);
+
+  if (product) {
     return (
-        <div>
-            <p>pagina de producto: {productId}</p>
-        </div>
-    )
-}
+      <Container maxW={"7xl"}>
+        <SimpleGrid
+          columns={{ base: 1, lg: 2 }}
+          spacing={{ base: 8, md: 10 }}
+          py={{ base: 18, md: 24 }}
+        >
+          <Flex>
+            <Image
+              rounded={"md"}
+              alt={productId.name}
+              src={product.image}
+              fit={"cover"}
+              align={"center"}
+              w={"100%"}
+              h={{ base: "100%", sm: "400px", lg: "500px" }}
+            />
+          </Flex>
+          <Stack spacing={{ base: 6, md: 10 }}>
+            <Box as={"header"}>
+              <Heading
+                lineHeight={1.1}
+                fontWeight={600}
+                fontSize={{ base: "2xl", sm: "4xl", lg: "5xl" }}
+              >
+                {product.name}
+              </Heading>
+              <Text
+                color={useColorModeValue("gray.900", "gray.400")}
+                fontWeight={300}
+                fontSize={"2xl"}
+              >
+                ${product.price}
+              </Text>
+            </Box>
 
-export default ProductItem
+            <Stack
+              spacing={{ base: 4, sm: 6 }}
+              direction={"column"}
+              divider={
+                <StackDivider
+                  borderColor={useColorModeValue("gray.200", "gray.600")}
+                />
+              }
+            >
+              <VStack spacing={{ base: 4, sm: 6 }}>
+                <Text fontSize={"lg"}>{product.attributes.description}</Text>
+              </VStack>
+              <Box>
+                <Text
+                  fontSize={{ base: "16px", lg: "18px" }}
+                  color={useColorModeValue("yellow.500", "yellow.300")}
+                  fontWeight={"500"}
+                  textTransform={"uppercase"}
+                  mb={"4"}
+                >
+                  Features
+                </Text>
+
+                <SimpleGrid columns={{ base: 1, md: 2 }} spacing={10}>
+                  <List spacing={2}>
+                    <ListItem>{product.attributes.shape}</ListItem>
+                    <ListItem>{product.attributes.hardiness}</ListItem>
+                  </List>
+                  <List spacing={2}>
+                    <ListItem>{product.attributes.taste}</ListItem>
+                  </List>
+                </SimpleGrid>
+              </Box>
+            </Stack>
+          </Stack>
+        </SimpleGrid>
+      </Container>
+    );
+  } else {
+    return <Spinner />;
+  }
+}
