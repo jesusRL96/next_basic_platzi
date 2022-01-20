087d888c3d076e7e3ec72621c90a73d51c05894e
jesus <jesus.alberto@desici.com>
2022-01-20 13:29:47 -0600

    medir performance

--- pages/_app.js
+++ pages/_app.js
@@ -1,6 +1,11 @@
 import { ChakraProvider } from '@chakra-ui/react'
 import Layout from '@components/Layout/Layout'
 
+
+export function reportWebVitals(metric) {
+  console.log(metric);
+}
+
 function MyApp({ Component, pageProps }) {
   return (
     <ChakraProvider>
